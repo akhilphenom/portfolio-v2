615a655afe65d2b180b8d643fdfc0b98f00bab42
Sai Akhil Katukam <saiakhilk.katukam@gmail.com>
2024-08-27 00:36:52 +0530

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { LampDemo } from "./components/ui/lamp-header"
+
+function App() {
+  return (
+    <main className="bg-slate-950">
+      <LampDemo/>
+    </main>
+  )
+}
+
+export default App
